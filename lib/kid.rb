5f825fc59a37be2e13f6a785dc7dfcdf72498107
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-08 20:17:57 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,15 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
+# class Kid
   
-  extend MetaDancing
+#   extend MetaDancing
   
-  include Dance
+#   include Dance
  
-  attr_accessor :name
+#   attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
-end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
